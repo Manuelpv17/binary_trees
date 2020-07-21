305444d4d39976d51de6f1f700670f3eb1ea7015
Manuelpv17 <manuelpv17@outlook.com>
2020-07-21 10:03:59 -0500

    Checked betty style

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -2,7 +2,7 @@
 
 /**
  * binary_tree_is_full - full binary tree in which all nodes
- * have either zero or two child nodes. 
+ * have either zero or two child nodes.
  * @tree: pointer to the root node of the tree to check
  * Return: 1 if full, 0 otherwise
  * If tree is NULL, your function must return 0
@@ -15,7 +15,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
+	if ((tree->left == NULL && tree->right != NULL) ||
+		(tree->left != NULL && tree->right == NULL))
 		return (0);
 
 	if (tree->left != NULL)
