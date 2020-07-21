7de4aa6fc041300153e7c70fbf965ea69c7c962c
Manuelpv17 <manuelpv17@outlook.com>
2020-07-20 20:23:28 -0500

    Check betty style

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -2,7 +2,8 @@
 /**
  * binary_tree_preorder - goes through a binary tree using pre-order traversal
  * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node. The value in the node must be passed as a parameter to this function
+ * @func: pointer to a function to call for each node
+ * The value in the node must be passed as a parameter to this function
  * If tree or func is NULL, do nothing
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
