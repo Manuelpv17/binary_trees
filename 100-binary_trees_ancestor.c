13da9a75457d3ca95c81d11b615b5f3a51153641
Manuelpv17 <manuelpv17@outlook.com>
2020-07-21 13:48:02 -0500

    Add task 19 common ancestor new logic

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,8 +1,5 @@
 #include "binary_trees.h"
 
-binary_tree_t *ancestor_recursion(const binary_tree_t *root, const binary_tree_t *first, const binary_tree_t *second);
-int ancestor_recursion_2(const binary_tree_t *root, const binary_tree_t *first, const binary_tree_t *second, int count);
-
 /**
  * binary_trees_ancestor - finds the lowest common ancestor of two nodes
  * @first: pointer to the first node
@@ -10,44 +7,25 @@ int ancestor_recursion_2(const binary_tree_t *root, const binary_tree_t *first, 
  * Return: pointer to the lowest common ancestor node of the two given nodes
  * If no common ancestor was found, your function must return NULL
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									 const binary_tree_t *second)
 {
-	const binary_tree_t *root = NULL;
+	const binary_tree_t *aux_first = first;
+	const binary_tree_t *aux_second = second;
 
 	if (first == NULL || second == NULL)
 		return (NULL);
 
-	root = first;
-	while (root->parent != NULL)
-		root = root->parent;
-
-	return (ancestor_recursion(root, first, second));
-}
-
-binary_tree_t *ancestor_recursion(const binary_tree_t *root, const binary_tree_t *first, const binary_tree_t *second)
-{
-	binary_tree_t *ancestor = NULL;
-
-	if (root->left != NULL)
-		ancestor = ancestor_recursion(root->left, first, second);
-	if (root->right != NULL)
-		ancestor = ancestor_recursion(root->right, first, second);
-
-	if (ancestor_recursion_2(root, first, second, 0) == 2 && ancestor == NULL)
-		return ((binary_tree_t *)root);
-
-	return (ancestor);
-}
-
-int ancestor_recursion_2(const binary_tree_t *root, const binary_tree_t *first, const binary_tree_t *second, int count)
-{
-	if (root->left != NULL)
-		count = ancestor_recursion_2(root->left, first, second, count);
-	if (root->right != NULL)
-		count = ancestor_recursion_2(root->right, first, second, count);
-
-	if (root == first || root == second)
-		count++;
-
-	return (count);
+	while (aux_first != NULL)
+	{
+		aux_second = second;
+		while (aux_second != NULL)
+		{
+			if (aux_first == aux_second)
+				return ((binary_tree_t *)aux_first);
+			aux_second = aux_second->parent;
+		}
+		aux_first = aux_first->parent;
+	}
+	return (NULL);
 }
