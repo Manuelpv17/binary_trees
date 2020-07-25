8c736e7583a77b1a41401dce1da24de79a833b6e
Manuelpv17 <manuelpv17@outlook.com>
2020-07-24 22:25:07 -0500

    Add task 31 insert avl

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -3,17 +3,43 @@
 bst_t *bst_insert(bst_t **tree, int value);
 /**
  * avl_insert - inserts a value in an AVL Tree
- * @tree: double pointer to the root node of the AVL tree for inserting the value
+ * @tree: double pointer to the root node of the AVL tree
  * @value: the value to store in the node to be inserted
  * Return:  pointer to the created node, or NULL on failure
- * If the address stored in tree is NULL, the created node must become the root node.
+ * If the address stored in tree is NULL,
+ * the created node must become the root node.
  * The resulting tree after insertion, must be a balanced AVL Tree
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	return (bst_insert(tree, value));
-}
+	avl_t *aux = NULL;
+	avl_t *new = NULL;
+	int balance = 0;
+	int prev_balance = 0;
 
+	new = bst_insert(tree, value);
+	aux = new;
+	while (aux != NULL)
+	{
+		prev_balance = balance;
+		balance = binary_tree_balance(aux);
+		if (balance > 1)
+		{
+			if (prev_balance < 0)
+				binary_tree_rotate_left(aux->left);
+			aux = binary_tree_rotate_right(aux);
+		}
+		else if (balance < -1)
+		{
+			if (prev_balance > 0)
+				binary_tree_rotate_right(aux->right);
+			aux = binary_tree_rotate_left(aux);
+		}
+		aux = aux->parent;
+	}
+
+	return (new);
+}
 /**
  * bst_insert - Inserts a value in a Binary Search Tree
  * @tree: double pointer to the root node of the BST to insert the value
