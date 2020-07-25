087ae4d26a0421bb51eb2f8cd29dfed17edcccaa
Manuelpv17 <manuelpv17@outlook.com>
2020-07-24 22:55:38 -0500

    Add task 123 inicial logic

--- 123-avl_remove.c
+++ 123-avl_remove.c
@@ -1,5 +1,7 @@
 #include "binary_trees.h"
 
+bst_t *bst_search(const bst_t *tree, int value);
+
 /**
  * avl_remove - removes a node from an AVL tree
  * @root: pointer to the root node of the tree for removing a node
@@ -59,8 +61,33 @@ avl_t *avl_remove(avl_t *root, int value)
 			aux = aux->left;
 		val = aux->n;
 
-		bst_remove(root, val);
+		avl_remove(root, val);
 		remove_me->n = val;
 	}
 	return (root);
+}
+
+/**
+ * bst_search - searches for a value in a Binary Search Tree
+ * @tree: pointer to the root node of the BST to search
+ * @value: value to search in the tree
+ * Return: pointer to the node containing a value equals to value
+ * If tree is NULL or if nothing is found, return NULL
+ */
+bst_t *bst_search(const bst_t *tree, int value)
+{
+	if (tree == NULL)
+		return (NULL);
+
+	while (1)
+	{
+		if (tree->n == value)
+			return ((bst_t *)tree);
+		if (tree->right != NULL && value > tree->n)
+			tree = tree->right;
+		else if (tree->left != NULL && value < tree->n)
+			tree = tree->left;
+		else
+			return (NULL);
+	}
 }
